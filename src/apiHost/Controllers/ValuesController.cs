3a0e549a5b0ff3c7acbd602effb70cdb9f7f9240
Herb Stahl <ghstahl@gmail.com>
2018-05-01 16:55:19 -0700

    apiHost gated by identityserver4

---
+++ src/apiHost/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiHost.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        [Route("echo")]
+        public string Echo(string message)
+        {
+            return message;
+        }
+        [HttpGet]
+        [Authorize]
+        [Route("echo2")]
+        public string Echo2(string message)
+        {
+            return message;
+        }
+    }
+}
