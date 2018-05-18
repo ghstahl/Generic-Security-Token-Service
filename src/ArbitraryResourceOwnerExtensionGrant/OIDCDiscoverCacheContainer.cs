02b00699ff446e5d84ef1d6651cab71004964f38
Herb Stahl <ghstahl@gmail.com>
2018-05-18 12:07:42 -0700

    missing

---
+++ src/ArbitraryResourceOwnerExtensionGrant/OIDCDiscoverCacheContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using IdentityModel.Client;
+using IdentityModelExtras;
+using Microsoft.Extensions.Configuration;
+
+namespace ArbitraryResourceOwnerExtensionGrant
+{
+    public class OIDCDiscoverCacheContainer : ConfiguredDiscoverCacheContainer
+    {
+        public OIDCDiscoverCacheContainer(IConfiguration configuration) : base(configuration, "self")
+        {
+        }
+    }
+}
