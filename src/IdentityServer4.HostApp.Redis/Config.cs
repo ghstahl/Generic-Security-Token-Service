e2d9aaa3e8aca5847714580a705753c767ad4b38
ghstahl@gmail.com <ghstahl@gmail.com>
2018-05-19 17:55:12 -0700

    added redis variant that persists grants

---
+++ src/IdentityServer4.HostApp.Redis/Config.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using IdentityServer4Extras;
+
+namespace IdentityServer4.HostApp
+{
+    public class Config
+    {
+        // scopes define the API resources in your system
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("nitro", "nitro"),
+                new ApiResource("metal", "metal")
+            };
+        }
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+        private static int AccessTokenLifetimeMax => 60 * 60 * 24 * 30;// 30 day
+
+        private static int AbsoluteRefreshTokenLifetimeMax => 60 * 60 * 24 * 30 * 12;// 1 yearish
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // client credentials client
+            return new List<Client>
+            {
+                // arbitrary resource owner grant client
+                new Client
+                {
+                    ClientId = "arbitrary-resource-owner-client",
+                    AllowedGrantTypes = new[]
+                    { 
+                        ArbitraryResourceOwnerExtensionGrant.Constants.ArbitraryResourceOwner,
+                        ArbitraryOpenIdConnectTokenExtensionGrants.Constants.ArbitraryOIDCResourceOwner,
+                        ArbitraryNoSubjectExtensionGrant.Constants.ArbitraryNoSubject,
+                        GrantType.Implicit
+                    },
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {
+                        "nitro",
+                        "metal",
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile },
+                    RequireClientSecret = false,
+                    AccessTokenLifetime = AccessTokenLifetimeMax,//this is the default if not pased in, and the upperrange.
+                    AbsoluteRefreshTokenLifetime =AbsoluteRefreshTokenLifetimeMax,
+                    ClientClaimsPrefix = null,
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                }
+            };
+        }
+        
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/IdentityServer4.HostApp.Redis/Startup.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ArbitraryNoSubjectExtensionGrant.Extensions;
+using ArbitraryOpenIdConnectTokenExtensionGrants.Extensions;
+using ArbitraryResourceOwnerExtensionGrant;
+using ArbitraryResourceOwnerExtensionGrant.Extensions;
+using IdentityServer4Extras;
+using IdentityServer4Extras.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using MultiRefreshTokenSameSubjectSameClientIdWorkAround.Extensions;
+using ProfileServiceManager.Extensions;
+using Serilog;
+
+namespace IdentityServer4.HostApp.Redis
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public IConfiguration Configuration { get; private set; }
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            _hostingEnvironment = env;
+            StartupConfiguration(configuration);
+            StartupLogger();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<OIDCDiscoverCacheContainer>();
+
+            var builder = services
+                .AddIdentityServer(options => { options.InputLengthRestrictions.RefreshToken = 2048; })
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClientsExtra(Config.GetClients())
+                .AddTestUsers(Config.GetUsers())
+                .AddIdentityServer4Extras()
+                .AddProfileServiceManager()
+                .AddArbitraryOwnerResourceExtensionGrant()
+                .AddArbitraryOpenIdConnectTokenExtensionGrant()
+                .AddArbitraryNoSubjectExtensionGrant()
+                 
+                .AddOperationalStore(options =>
+                {
+                    options.RedisConnectionString = Configuration["redisConnectionString"];
+                    options.Db = 1;
+                })
+                .AddRedisCaching(options =>
+                {
+                    options.RedisConnectionString = Configuration["redisConnectionString"];
+                    options.KeyPrefix = "prefix";
+                });  
+            // my replacement services.
+
+            builder.AddRefreshTokenRevokationGeneratorWorkAround();
+
+            services.AddArbitraryNoSubjectExtentionGrantTypes();
+            services.AddArbitraryResourceOwnerExtentionGrantTypes();
+            services.AddArbitraryOpenIdConnectTokenExtensionGrantTypes();
+            services.AddIdentityServer4ExtraTypes();
+            services.AddRefreshTokenRevokationGeneratorWorkAroundTypes();
+
+            services.AddMemoryCache();
+            services.AddMvc();
+
+            services.AddLogging();
+            services.AddWebEncoders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+            app.UseMvc();
+        }
+        private void StartupLogger()
+        {
+            var appDataPath = Path.Combine(_hostingEnvironment.ContentRootPath, "App_Data");
+
+            var rollingPath = Path.Combine(_hostingEnvironment.ContentRootPath, "logs/myapp-{Date}.txt");
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.RollingFile(rollingPath)
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+            Log.Information("Ah, there you are!");
+        }
+
+        private void StartupConfiguration(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{_hostingEnvironment.EnvironmentName}.json", optional: true);
+
+            if (_hostingEnvironment.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+    }
+}
