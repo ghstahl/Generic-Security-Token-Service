531dcb75ac08f6fb4c03765fbea32d99a420a51b
Herb Stahl <ghstahl@gmail.com>
2018-06-07 05:11:39 -0700

    missing

---
+++ src/WebApplication2/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+using P7.AspNetCore.Identity.InMemory;
+
+namespace Microsoft.AspNetCore.Identity
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationRole : MemoryRole
+    {
+    }
+
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : MemoryUser
+    {
+    }
+}
