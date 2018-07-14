6658add1a23b1a2dfc6b3882073a6a7d1787a6f1
ghstahl@gmail.com <ghstahl@gmail.com>
2018-07-14 05:05:06 -0700

    added new test

--- src/Tests_ExtensionGrants/ArbitraryResourceOwnerTests.cs
+++ src/Tests_ExtensionGrants/ArbitraryResourceOwnerTests.cs
@@ -221,6 +221,40 @@ namespace Tests_ExtensionGrants
             result.RefreshToken.ShouldBeNull();
             result.ExpiresIn.ShouldNotBeNull();
         }
+        [TestMethod]
+        public async Task Mint_arbitrary_resource_owner()
+        {
+            var client = new TokenClient(
+                _server.BaseAddress + "connect/token",
+                ClientId,
+                _server.CreateHandler());
+
+            Dictionary<string, string> paramaters = new Dictionary<string, string>()
+            {
+                {OidcConstants.TokenRequest.ClientId, ClientId},
+                {OidcConstants.TokenRequest.ClientSecret, ClientSecret},
+                {OidcConstants.TokenRequest.GrantType, ArbitraryResourceOwnerExtensionGrant.Constants.ArbitraryResourceOwner},
+                {
+                    OidcConstants.TokenRequest.Scope,
+                    $"{IdentityServerConstants.StandardScopes.OfflineAccess} nitro metal"
+                },
+                {
+                    ArbitraryNoSubjectExtensionGrant.Constants.ArbitraryClaims,
+                    "{'role': ['application', 'limited'],'query': ['dashboard', 'licensing'],'seatId': ['8c59ec41-54f3-460b-a04e-520fc5b9973d'],'piid': ['2368d213-d06c-4c2a-a099-11c34adc3579']}"
+                },
+                {
+                    ArbitraryResourceOwnerExtensionGrant.Constants.Subject,
+                    "Ratt"
+                },
+                {ArbitraryNoSubjectExtensionGrant.Constants.AccessTokenLifetime, "3600"}
+            };
+
+            var result = await client.RequestAsync(paramaters);
+            result.AccessToken.ShouldNotBeNullOrEmpty();
+            result.RefreshToken.ShouldNotBeNull();
+            result.ExpiresIn.ShouldNotBeNull();
+ 
+        }
 
         [TestMethod]
         public async Task Mint_arbitrary_resource_owner_and_refresh()
