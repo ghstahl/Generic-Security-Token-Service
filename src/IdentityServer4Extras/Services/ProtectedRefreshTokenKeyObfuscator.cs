1ab7635bb663becba454f093ae6de0a208f1f738
ghstahl@gmail.com <ghstahl@gmail.com>
2018-10-16 09:31:57 -0700

    added a helper DataProtector/DataProtectorBase.cs

--- src/IdentityServer4Extras/Services/ProtectedRefreshTokenKeyObfuscator.cs
+++ src/IdentityServer4Extras/Services/ProtectedRefreshTokenKeyObfuscator.cs
@@ -4,18 +4,22 @@ using System.Threading.Tasks;
 using IdentityModel;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Logging;
+using P7.Core.DataProtector;
 using P7.Core.Utils;
 
 namespace IdentityServer4Extras.Services
 {
-    public class ProtectedRefreshTokenKeyObfuscator : IRefreshTokenKeyObfuscator
+
+    public class ProtectedRefreshTokenKeyObfuscator : DataProtectorBase, IRefreshTokenKeyObfuscator
     {
         private const string Purpose = "IdentityServer4Extras.Services.ProtectedRefreshTokenKeyObfuscator";
 
         private readonly IDataProtector _protector;
         private readonly ILogger _logger;
 
-        public ProtectedRefreshTokenKeyObfuscator(IDataProtectionProvider provider, ILogger<ProtectedRefreshTokenKeyObfuscator> logger)
+        public ProtectedRefreshTokenKeyObfuscator(
+            IDataProtectionProvider provider, ILogger<ProtectedRefreshTokenKeyObfuscator> logger):
+            base(provider, Purpose,logger )
         {
             _protector = provider.CreateProtector(Purpose);
             _logger = logger;
@@ -23,36 +27,12 @@ namespace IdentityServer4Extras.Services
 
         public Task<string> ObfuscateAsync(string key)
         {
-            Guard.ArgumentNotNull(nameof(key), key);
-            try
-            {
-                var bytes = Encoding.UTF8.GetBytes(key);
-                bytes = _protector.Protect(bytes);
-                var value = Base64Url.Encode(bytes);
-                return Task.FromResult(value);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Exception writing protected key");
-                throw;
-            }
+            return Task.FromResult(ProtectAsync(key).GetAwaiter().GetResult());
         }
 
         public Task<string> UnObfuscateAsync(string key)
         {
-            Guard.ArgumentNotNull(nameof(key), key);
-            try
-            {
-                var bytes = Base64Url.Decode(key);
-                bytes = _protector.Unprotect(bytes);
-                var value = Encoding.UTF8.GetString(bytes);
-                return Task.FromResult(value);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Exception reading protected key");
-                throw;
-            }
+            return Task.FromResult(UnprotectAsync(key).GetAwaiter().GetResult());
         }
     }
 }
---
+++ src/P7.Core/DataProtector/DataProtectorBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Logging;
+ 
+namespace P7.Core.DataProtector
+{
+    public abstract class DataProtectorBase
+    {
+        private readonly IDataProtector _protector;
+        private readonly ILogger _logger;
+
+        public DataProtectorBase(IDataProtectionProvider provider, string purpose, ILogger logger)
+        {
+            _protector = provider.CreateProtector(purpose);
+            _logger = logger;
+        }
+        public Task<string> ProtectAsync(string key)
+        {
+            try
+            {
+                var value = _protector.Protect(key);
+                return Task.FromResult(value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception writing protected key");
+                throw;
+            }
+        }
+        public Task<string> UnprotectAsync(string key)
+        {
+            try
+            {
+                var value = _protector.Unprotect(key);
+                return Task.FromResult(value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception reading protected key");
+                throw;
+            }
+        }
+
+    }
+}
