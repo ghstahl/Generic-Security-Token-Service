c408e79cc3a9e3a277eaacdef11f7933a95688c3
ghstahl@gmail.com <ghstahl@gmail.com>
2018-10-22 10:17:20 -0700

    missing

---
+++ src/ProfileServiceManager/DefaultProfileServicePlugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.Extensions.Logging;
+
+namespace ProfileServiceManager
+{
+    public class DefaultProfileServicePlugin: IProfileServicePlugin,IProfileService
+    {
+        private ILogger Logger { get; }
+
+        public DefaultProfileServicePlugin(ILogger<DefaultProfileServicePlugin> logger)
+        {
+            Logger  = logger;
+        }
+        public string Name => "default_profile_service";
+        public IProfileService ProfileService => this;
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            Logger.LogDebug("IsActive called from: {caller}", context.Caller);
+            context.IsActive = true;
+            return Task.CompletedTask;
+        }
+    }
+}
