3de621f8ca79c09c3126977fca8ecb9656cbd55c
ghstahl@gmail.com <ghstahl@gmail.com>
2018-11-03 08:22:06 -0700

    initial baseline

---
+++ src/GenericSecurityTokenService.WebApp/Controllers/SummaryController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace GenericSecurityTokenService.WebApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class SummaryController : ControllerBase
+    {
+        private IActionContextAccessor _actionContextAccessor;
+        public SummaryController(IActionContextAccessor actionContextAccessor)
+        {
+            _actionContextAccessor = actionContextAccessor;
+        }
+        private static Dictionary<string, object> _output;
+
+        private static Dictionary<string, object> Output
+        {
+            get
+            {
+                if (_output == null)
+                {
+                    var credits = new Dictionary<string, string>()
+                    {
+                        {"IdentityServer4", "https://github.com/IdentityServer/IdentityServer4"},
+
+                    };
+                    _output = new Dictionary<string, object>
+                    {
+                        {"version", "1.0"},
+                        {"application", "GenericSecurityTokenService"},
+                        {"author", "Herb Stahl"},
+                        {"credits", credits},
+
+                    };
+                }
+                return _output;
+            }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IDictionary<string, object>> Get()
+        {
+            if (_output == null)
+            {
+                Output.Add("authority", _actionContextAccessor.ActionContext.HttpContext.GetIdentityServerHost());
+            }
+            return Output;
+        }
+    }
+
+ 
+}
