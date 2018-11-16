03e60650060e4cc7b4c5230f9941d349b1cc9108
Herb Stahl <ghstahl@gmail.com>
2018-11-15 16:11:02 -0800

    missing ratelimit files

---
+++ src/IdentityServer4.HostApp.Redis/RateLimiting/RateLimitingExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreRateLimit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer4.HostApp.RateLimiting
+{
+    public static class RateLimitingExtensions
+    {
+        public static void AddRateLimiting(this IServiceCollection services, IConfiguration configuration)
+        {
+            // needed to load configuration from appsettings.json
+            services.AddOptions();
+
+            //load general configuration from appsettings.json
+            services.Configure<ClientRateLimitOptions>(configuration.GetSection("ClientRateLimiting"));
+
+            //load client rules from appsettings.json
+            services.Configure<ClientRateLimitPolicies>(configuration.GetSection("ClientRateLimitPolicies"));
+
+
+            // inject counter and rules distributed cache stores
+            services.AddSingleton<IClientPolicyStore, DistributedCacheClientPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
+
+
+            services.AddSingleton<IClientRateLimiterHandler, ClientRateLimiterHandler>();
+
+        }
+    }
+}
