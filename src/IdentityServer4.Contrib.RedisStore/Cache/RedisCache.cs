31dcccfb489433b112adfcef26ca39fd501cd2bb
ghstahl@gmail.com <ghstahl@gmail.com>
2018-11-24 10:33:28 -0800

    IdentityServer4.Contrib.RedisStore needs to be linked to this identitserver

---
+++ src/IdentityServer4.Contrib.RedisStore/Cache/RedisCache.cs
@@ -0,0 +1,80 @@
+﻿using IdentityServer4.Services;
+using IdentityServer4.Stores.Serialization;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.Contrib.RedisStore.Cache
+{
+    /// <summary>
+    /// Redis based implementation for ICache<typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class RedisCache<T> : ICache<T> where T : class
+    {
+        private readonly IDatabase database;
+
+        private readonly RedisCacheOptions options;
+
+        private readonly ILogger<RedisCache<T>> logger;
+
+        public RedisCache(RedisMultiplexer<RedisCacheOptions> multiplexer, ILogger<RedisCache<T>> logger)
+        {
+            if (multiplexer is null)
+                throw new ArgumentNullException(nameof(multiplexer));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            this.options = multiplexer.RedisOptions;
+            this.database = multiplexer.Database;
+        }
+
+        private string GetKey(string key) => $"{this.options.KeyPrefix}{typeof(T).FullName}:{key}";
+
+        public async Task<T> GetAsync(string key)
+        {
+            var cacheKey = GetKey(key);
+            var item = await this.database.StringGetAsync(cacheKey).ConfigureAwait(false);
+            if (item.HasValue)
+            {
+                logger.LogDebug($"retrieved {typeof(T).FullName} with Key: {key} from Redis Cache successfully.");
+                return Deserialize(item);
+            }
+            else
+            {
+                logger.LogDebug($"missed {typeof(T).FullName} with Key: {key} from Redis Cache.");
+                return default(T);
+            }
+        }
+
+        public async Task SetAsync(string key, T item, TimeSpan expiration)
+        {
+            var cacheKey = GetKey(key);
+            await this.database.StringSetAsync(cacheKey, Serialize(item), expiration).ConfigureAwait(false);
+            logger.LogDebug($"persisted {typeof(T).FullName} with Key: {key} in Redis Cache successfully.");
+        }
+
+        #region Json
+        private JsonSerializerSettings SerializerSettings
+        {
+            get
+            {
+                var settings = new JsonSerializerSettings();
+                settings.Converters.Add(new ClaimConverter());
+                return settings;
+            }
+        }
+
+        private T Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, this.SerializerSettings);
+        }
+
+        private string Serialize(T item)
+        {
+            return JsonConvert.SerializeObject(item, this.SerializerSettings);
+        }
+        #endregion
+    }
+}
---
+++ src/IdentityServer4.Contrib.RedisStore/Extensions/RedisOptions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace IdentityServer4.Contrib.RedisStore
+{
+    /// <summary>
+    /// Represents Redis general options.
+    /// </summary>
+    public abstract class RedisOptions
+    {
+        /// <summary>
+        ///Configuration options objects for StackExchange.Redis Library.
+        /// </summary>
+        public ConfigurationOptions ConfigurationOptions { get; set; }
+
+        /// <summary>
+        /// Connection String for connecting to Redis Instance.
+        /// </summary>
+        public string RedisConnectionString { get; set; }
+
+        /// <summary>
+        ///The specific Db number to connect to, default is -1.
+        /// </summary>
+        public int Db { get; set; } = -1;
+
+        private string _keyPrefix = string.Empty;
+
+        /// <summary>
+        /// The Prefix to add to each key stored on Redis Cache, default is Empty.
+        /// </summary>
+        public string KeyPrefix
+        {
+            get
+            {
+                return string.IsNullOrEmpty(this._keyPrefix) ? this._keyPrefix : $"{_keyPrefix}:";
+            }
+            set
+            {
+                this._keyPrefix = value;
+            }
+        }
+
+        internal RedisOptions()
+        {
+            this.multiplexer = GetConnectionMultiplexer();
+        }
+
+        private Lazy<IConnectionMultiplexer> GetConnectionMultiplexer()
+        {
+            return new Lazy<IConnectionMultiplexer>(
+                () => string.IsNullOrEmpty(this.RedisConnectionString)
+                    ? ConnectionMultiplexer.Connect(this.ConfigurationOptions)
+                    : ConnectionMultiplexer.Connect(this.RedisConnectionString));
+        }
+
+        private Lazy<IConnectionMultiplexer> multiplexer = null;
+
+        internal IConnectionMultiplexer Multiplexer => this.multiplexer.Value;
+    }
+
+    /// <summary>
+    /// Represents Redis Operational store options.
+    /// </summary>
+    public class RedisOperationalStoreOptions : RedisOptions
+    {
+
+    }
+
+    /// <summary>
+    /// Represents Redis Cache options.
+    /// </summary>
+    public class RedisCacheOptions : RedisOptions
+    {
+
+    }
+}
