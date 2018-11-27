aa41241563d6977b6ece8752a900c1ebcd9b1135
ghstahl@gmail.com <ghstahl@gmail.com>
2018-11-27 09:34:37 -0800

    missing

---
+++ src/KeyVaultTokenCreateService/ITokenSigningCredentialStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.Azure.KeyVault.Models;
+
+namespace P7IdentityServer4
+{
+    public interface ITokenSigningCredentialStore 
+    {
+        Task<CertificateBundle> GetCertificateBundleAsync();
+    }
+}
