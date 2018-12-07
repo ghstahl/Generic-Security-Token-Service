69a2044a51da66b926f3dc0ea3a389f4d745ed24
ghstahl@gmail.com <ghstahl@gmail.com>
2018-12-07 13:20:51 -0800

    added a home api to show life

---
+++ src/WizardAppApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WizardAppApi.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"Hello", "There!"};
+        }
+    }
+}
